f833a0cf98aa5104d3080bcaa022ce4b10351a57
윤지 <yoonhihi97@gmail.com>
2021-09-29 19:21:04 +0900

    feat: add Feeds Tab

--- Components/AppNavigation/HomeTab.js
+++ Components/AppNavigation/HomeTab.js
@@ -1,14 +1,34 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import Header from "../Header";
-
+import Feeds from "../Feeds";
+import { Icon, Container, Content} from 'native-base';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 const HomeTab = ({ navigation }) =>{
     return(
-        <View>
+        <View style={styles.container}>
             <Header/>
-            <Text>HomeTab</Text>
+            <ScrollView>
+                <SafeAreaProvider>
+                    <Feeds/>
+                </SafeAreaProvider>
+            </ScrollView>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:"#fff"
+    },
+    storyWrap:{
+        flex:1,
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        paddingHorizontal:7,
+        borderBottomColor:"#eee",
+        borderBottomWidth:1
+    }
+})
 export default HomeTab;
